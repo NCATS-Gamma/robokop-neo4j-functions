35f3db3aea80c5ff92a7698c9c7afc99f5144d06
patrickkwang <patrick@covar.com>
2018-06-27 14:40:40 -0400

    switch to simplified graph to handle duplicate-concept issues

--- src/main/java/robokop/Traversable.java
+++ src/main/java/robokop/Traversable.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserFunction;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
 
 /**
  * Determine if a graph is traversable from a given set of 'labeled' nodes.
@@ -18,9 +16,9 @@ import org.neo4j.graphdb.Relationship;
 public class Traversable
 {
     private void recurse(
-            Map<Long, List<Relationship>> edges,
-            Map<Long, Boolean> reachable,
-            Long node_id) {
+            Map<String, List<List<String>>> edges,
+            Map<String, Boolean> reachable,
+            String node_id) {
 
         // If node has already been visited, quit recursing.
         if (reachable.get(node_id)) {
@@ -31,11 +29,11 @@ public class Traversable
         reachable.put(node_id, true);
 
         // Get edges starting at node.
-        List<Relationship> edges_from_node = edges.get(node_id);
+        List<List<String>> edges_from_node = edges.get(node_id);
 
         // Mark downstream neighbors as reachable.
-        for (Relationship e : edges_from_node) {
-            recurse(edges, reachable, e.getEndNode().getId());
+        for (List<String> e : edges_from_node) {
+            recurse(edges, reachable, e.get(1));
         }
 
         return;
@@ -44,28 +42,29 @@ public class Traversable
     @UserFunction
     @Description("robokop.traversable([n0,n1,...], [r0,r1,...], [m0,m1,...]) - is the graph (n,r) traversable given labeled nodes m.")
     public Boolean traversable(
-            @Name("nodes") List<Node> nodes,
-            @Name("edges") List<Relationship> edges,
-            @Name("labeled") List<Node> labeled) {
+            @Name("nodes") List<String> nodes,
+            @Name("edges") List<List<String>> edges,
+            @Name("labeled") List<String> labeled) {
 
         // Initialize node->edges map and node->reachable map.
-        Map<Long, List<Relationship>> edges_by_nodes = new HashMap();
-        Map<Long, Boolean> reachable = new HashMap();
-        for (Node n : nodes) {
-            edges_by_nodes.put((Long)n.getId(), new ArrayList());
-            reachable.put((Long)n.getId(), false);
+        Map<String, List<List<String>>> edges_by_nodes = new HashMap();
+        Map<String, Boolean> reachable = new HashMap();
+        for (String n : nodes) {
+            edges_by_nodes.put(n, new ArrayList());
+            reachable.put(n, false);
         }
 
         // Populate node->edges map, sorting edges by start node.
-        for (Relationship e : edges) {
-            edges_by_nodes.get(e.getStartNodeId()).add(e);
+        for (List<String> e : edges) {
+            edges_by_nodes.get(e.get(0)).add(e);
         }
 
         // Run recursion to populate reachable
-        for (Node n : labeled) {
-            recurse(edges_by_nodes, reachable, n.getId());
+        for (String n : labeled) {
+            recurse(edges_by_nodes, reachable, n);
         }
 
+        // AND the reachability for all nodes
         return reachable.values().stream().reduce(true, (a,b)->a&&b);
     }
 }
